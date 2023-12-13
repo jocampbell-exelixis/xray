d1c0fcecbadaf663238b0dca0aecf9b1d5711e7e
Joshua Campbell <jocampbell@exelixis.com>
2023-12-13 13:01:03 -0800

    On node config script update

---
+++ on-node-conf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MOGUL_ACTIVATION_ID=$2
+
+# Customize for Global Phasing
+echo "Setting up Global Phasing Software..."
+echo 'source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh' >> /home/slurm/.profile
+echo 'source /opt/GPhL/ALL_snapshot_20230222/setup.sh' >> /home/slurm/.profile
+echo 'export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"' >> /home/slurm/.profile
+echo 'export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul' >> /home/slurm/.profile
+echo 'source /opt/hkl/HKL2000_v722-Linux-x86_64/hkl_setup.sh' >> /home/slurm/.profile
+echo 'source /usr/local/phenix-1.20.1-4487/phenix_env.sh' >> /home/slurm/.profile
+echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/slurm/.bashrc
+echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/ubuntu/.bashrc
+/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID
+export PATH="/opt/pymol/pymol/bin:$PATH"
+export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul
+export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"
+source /opt/GPhL/ALL_snapshot_20230222/setup.sh
+source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh
+source /opt/hkl/HKL2000_v722-Linux-x86_64/hkl_setup.sh
+source /usr/local/phenix-1.20.1-4487/phenix_env.sh
+echo "Completed setup of Global Phasing Software!"
