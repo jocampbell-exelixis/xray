b4c705461f1b759a63f802b3831c7a2645dd8ce0
Joshua Campbell <jocampbell@exelixis.com>
2024-04-26 19:50:09 -0700

    Bumping cuda install to 12.4

--- on-node-conf.sh
+++ on-node-conf.sh
@@ -27,3 +27,17 @@ source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh
 source /opt/hkl/HKL2000_v722-Linux-x86_64/hkl_setup.sh
 source /usr/local/phenix-1.20.1-4487/phenix_env.sh
 echo "Completed setup of Global Phasing Software!"
+
+CUDA_INSTALL_PATH=/shared/cuda
+CUDA_VERSION=12.4.1
+CUDA_LONG_VERSION=12.4.1_550.54.15
+
+# Install CUDA Toolkit
+cd "${CUDA_INSTALL_PATH}" || return
+sudo wget "https://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/local_installers/cuda_${CUDA_LONG_VERSION}_linux.run"
+sudo sh cuda_"${CUDA_LONG_VERSION}"_linux.run --defaultroot="${CUDA_INSTALL_PATH}" --toolkit --toolkitpath="${CUDA_INSTALL_PATH}"/"${CUDA_VERSION}" --samples --silent
+sudo rm cuda_"${CUDA_LONG_VERSION}"_linux.run
+
+sudo out="/etc/profile.d/cuda.sh" sh -c "cat << EOF > \"\$out\"
+\$PATH:${CUDA_INSTALL_PATH}/${CUDA_VERSION}/bin
+EOF"
