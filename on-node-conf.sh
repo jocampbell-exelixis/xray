42804607a0ce8447870e5dc844b35483d6f66fc1
Joshua Campbell <jocampbell@exelixis.com>
2024-04-30 13:25:53 -0700

    Need to add groups

--- on-node-conf.sh
+++ on-node-conf.sh
@@ -8,8 +8,11 @@ S3Bucket='parallelcluster-d1db3a7e7a2cdb9c-v1-do-not-delete'
 sudo aws s3 cp s3://$S3Bucket/xray-software/openeye/oe_license.txt /opt/openeye
 sudo chown -R ubuntu:ubuntu /opt/openeye/oe_license.txt
 
-sudo useradd -m -u 1001 -d /home/jmurray -s /bin/bash jmurray
-sudo useradd -m -u 1022 -d /home/jsanders -s /bin/bash jsanders
+sudo groupadd -g 1001 jmurray
+sudo groupadd -g 1022 jsanders
+
+sudo useradd -m -u 1022 -g 1022 -d /home/jmurray -s /bin/bash jmurray
+sudo useradd -m -u 1022 -g 1022 -d /home/jsanders -s /bin/bash jsanders
 
 # Customize for Global Phasing
 echo "Setting up Global Phasing Software..."
